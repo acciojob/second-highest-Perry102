dfa4e67ab87b23e8b38d324cc08fc6a882c995c9
Perry102 <141802227+Perry102@users.noreply.github.com>
2024-11-02 20:08:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+        //Write your code here
+        if (arr.length <= 1) {
+          return -Infinity;
+        }
+        let highest = -Infinity;
+        let secondHighest = -Infinity;
+        for (let i = 0; i < arr.length; i++) {
+          if (arr[i] > highest) {
+            secondHighest = highest;
+            highest = arr[i];
+          } else if (arr[i] > secondHighest && arr[i] < highest) {
+            secondHighest = arr[i];
+          }
+        }
+        if (highest === secondHighest) {
+          return -Infinity;
+        }
+        return secondHighest;
+      }
